d2506bc265618cc37d596b8d201b2a57c8d582ad
sabeet <sabeet.other.way@gmail.com>
2023-12-02 01:41:38 -0500

    Create quotes.sql

---
+++ quotes.sql
@@ -0,0 +1,62 @@
+CREATE TABLE quotes (
+    quote_english VARCHAR(255),
+    quote_bangla VARCHAR(255),
+    quote_spanish VARCHAR(255),
+    quote_arabic VARCHAR(255),
+    quote_hindi VARCHAR(255),
+    quote_japanese VARCHAR(255),
+    definition VARCHAR(255)
+);
+
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Conscience', 'বিবেক', 'Conciencia', 'الضمير', 'अंतरात्मा की आवाज', '良心');
+
+-- 2023-12-01 18:20:19.6210
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Humanity', 'মানবতা', 'Humanidad', 'إنسانية', 'इंसानियत', '人類');
+
+-- 2023-12-01 18:20:19.6620
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Determination', 'সংকল্প', 'Determinación', 'عزيمة', 'दृढ़ निश्चय', '決定');
+
+-- 2023-12-01 18:20:19.7000
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Liberty', 'স্বাধীনতা', 'Libertad', 'حرية', 'स्वतंत्रता', '自由');
+
+-- 2023-12-01 18:20:19.7480
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Strength', 'শক্তি', 'Fortaleza', 'قوة', 'ताकत', '強さ');
+
+-- 2023-12-01 18:20:19.8080
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Power', 'শক্তি', 'Fuerza', 'قوة', 'शक्ति', '力');
+
+-- 2023-12-01 18:20:19.8540
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Calm', 'শান্ত', 'Calma', 'هادئ', 'शांत', '落ち着いた');
+
+-- 2023-12-01 18:20:19.9170
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Fire', 'আগুন', 'Fuego', 'نار', 'आग', '火');
+
+-- 2023-12-01 18:20:19.9560
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Water', 'জল', 'Agua', 'ماء', 'पानी', '水');
+
+-- 2023-12-01 18:20:19.9970
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Earth', 'পৃথিবী', 'Tierra', 'أرض', 'धरती', '地球');
+
+-- 2023-12-01 18:20:20.0690
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Insight', 'অন্তর্দৃষ্টি', 'Conocimiento', 'بصيرة', 'अंतर्दृष्टि', '洞察力');
+
+-- 2023-12-01 18:20:20.1090
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Empathy', 'সহমর্মিতা', 'Empatía', 'تعاطف', 'समानुभूति', '共感');
+
+-- 2023-12-01 18:20:20.1700
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Integrity', 'অখণ্ডতা', 'Integridad', 'نزاهة', 'अखंडता', '誠実さ');
+
+-- 2023-12-01 18:20:20.2140
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Growth', 'বৃদ্ধি', 'Crecimiento', 'نمو', 'विकास', '成長');
+
+-- 2023-12-01 18:20:20.2820
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Self-Awareness', 'আত্ম-সচেতনতা', 'Conciencia de sí mismo', 'الوعي الذاتي', 'स्व जागरूकता', '自己認識');
+
+-- 2023-12-01 18:20:20.4110
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Learning', 'শেখা', 'Aprendiendo', 'تعلُّم', 'सीखना', '学ぶ');
+
+-- 2023-12-01 18:20:20.5010
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Connection', 'সংযোগ', 'Conexión', 'اتصال', 'संबंध', '繋がり');
+
+-- 2023-12-01 18:20:20.5460
+INSERT INTO quotes (quote_english, quote_bangla, quote_spanish, quote_arabic, quote_hindi, quote_japanese) VALUES ('Compassion', 'করুণা', 'Compasión', 'عطف', 'करुणा', '思いやり');
